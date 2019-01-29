f6cb57c4304ad75018ef90cbecffff2a6d40c89a
Lyon J Till <lyon_till@outlook.com>
2019-01-29 17:46:19 -0600

    Updated AzureResourceGroups/

--- Functions/AzureResourceGroups/run.ps1
+++ Functions/AzureResourceGroups/run.ps1
@@ -1,9 +1,13 @@
+#Requires -Modules @{ ModuleName = "Az.Resources"; ModuleVersion = "1.1.0" }
+
 param ($req, $TriggerMetadata)
+
+$cmdletParameters = $req.Query
+
+$cmdletParameters.ErrorAction = "Stop"
 
 Write-Verbose -Message "PowerShell HTTP trigger function processed a request." -Verbose
 
-$status = 200
-
 Push-OutputBinding -Name res -Value ([HttpResponseContext]@{
-    StatusCode = $status
+    StatusCode = 200
 })
