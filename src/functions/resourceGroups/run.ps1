7f99139dfc25b9689eb9bb65edda3c32027c804e
Lyon J Till <lyon_till@outlook.com>
2019-01-29 18:21:46 -0600

    Updated resourceGroup

--- src/functions/resourceGroups/run.ps1
+++ src/functions/resourceGroups/run.ps1
@@ -3,10 +3,38 @@
 param ($req, $TriggerMetadata)
 
 $cmdletParameters = $req.Query
-
 $cmdletParameters.ErrorAction = "Stop"
 
-Write-Verbose -Message "PowerShell HTTP trigger function processed a request." -Verbose
+function Invoke-Trigger {
+
+    param (
+        [Parameter()]
+        $req,
+
+        [Parameter()]
+        $TriggerMetadata
+    )
+
+    begin {
+        Write-Verbose -Message "PowerShell HTTP trigger function processed a request." -Verbose
+    
+        $pass = ConvertTo-SecureString $env:SECRET -AsPlainText -Force
+        $cred = New-Object -TypeName pscredential -ArgumentList $env:APPLICATIONID, $pass
+        Connect-AzAccount -ServicePrincipal -Tenant $env:TENANTID -Credential $cred
+    }
+    
+    process {
+        $pass = ConvertTo-SecureString $env:SECRET -AsPlainText -Force
+        $cred = New-Object -TypeName pscredential -ArgumentList $env:APPLICATIONID, $pass
+        Connect-AzAccount -ServicePrincipal -Tenant $env:TENANTID -Credential $cred
+    }
+    
+    end {
+        Write-Verbose -Message "PowerShell HTTP trigger function completed a request." -Verbose
+    }
+}
+
+Invoke-Trigger -req $req -TriggerMetadata $TriggerMetadata
 
 Push-OutputBinding -Name res -Value ([HttpResponseContext]@{
     StatusCode = 200
