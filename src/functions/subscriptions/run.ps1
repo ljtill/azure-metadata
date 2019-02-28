2a60a87089357226bb5de4e1641a2464e7ce4866
Lyon J Till <lyon_till@outlook.com>
2019-02-28 12:43:57 -0600

    Added subscriptions/

---
+++ src/functions/subscriptions/run.ps1
@@ -0,0 +1,58 @@
+#Requires -Modules @{ ModuleName = "Az.Resources"; ModuleVersion = "1.1.0" }
+
+param ($req, $TriggerMetadata)
+
+$cmdletParameters = $req.Query
+$cmdletParameters.ErrorAction = "Stop"
+
+function Invoke-Trigger {
+
+    param (
+        [Parameter()]
+        $req,
+
+        [Parameter()]
+        $TriggerMetadata
+    )
+
+    begin {
+        Write-Verbose -Message "PowerShell HTTP trigger function processed a request." -Verbose
+        try {
+            Write-Verbose -Message "Azure authentication processed a request." -Verbose
+            #$azureClientSecret = ConvertTo-SecureString $env:AzureClientSecret -AsPlainText -Force
+            #$azureCredential = New-Object -TypeName pscredential -ArgumentList $env:AzureClientId, $azureClientSecret
+            #Connect-AzAccount -ServicePrincipal -Tenant $env:AzureTenantId -Credential $azureCredential | Out-Null
+            Write-Verbose -Message "Azure authentication completed a request." -Verbose
+        }
+        catch {
+            Write-Warning -Message "PowerShell HTTP trigger function encountered an error."
+            Write-Error $_
+            return
+        }
+    }
+    
+    process {
+        try {
+            $subscriptions = Get-AzSubscription
+            $subscriptionNames = $subscriptions.Name
+            $subscriptionNames | ForEach-Object {
+                Write-Verbose -Message ("Subscription: " + $_) -Verbose
+            }
+        }
+        catch {
+            Write-Warning -Message "PowerShell HTTP trigger function encountered an error."
+            Write-Error $_
+            return
+        }
+    }
+    
+    end {
+        Write-Verbose -Message "PowerShell HTTP trigger function completed a request." -Verbose
+    }
+}
+
+Invoke-Trigger -req $req -TriggerMetadata $TriggerMetadata
+
+Push-OutputBinding -Name res -Value ([HttpResponseContext]@{
+    StatusCode = 200
+})
